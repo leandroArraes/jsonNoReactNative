cca1cb6cef69d6380a1148c5cbcfb3db2950d1ad
leandroArraes <leandro.arraes.182@gmail.com>
2023-09-04 20:10:35 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {script} from './src/view/home/script'
+
+ const data = {
+  "cep": "20030-041",
+  "logradouro": "Rua Santa Luzia",
+  "complemento": "de 407 ao fim - lado ímpar",
+  "bairro": "Centro",
+  "localidade": "Rio de Janeiro",
+  "uf": "RJ",
+  "ibge": "3304557",
+  "gia": "",
+  "ddd": "21",
+  "siafi": "6001"
+ }
+
+
+
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Text>Cep: {data.cep}</Text>
+        <Text>Bairro: {data.bairro}</Text>
+
     </View>
   );
 }
---
+++ src/view/home/script.js
@@ -0,0 +1,12 @@
+export const data = {
+    "cep": "20030-041",
+    "logradouro": "Rua Santa Luzia",
+    "complemento": "de 407 ao fim - lado ímpar",
+    "bairro": "Centro",
+    "localidade": "Rio de Janeiro",
+    "uf": "RJ",
+    "ibge": "3304557",
+    "gia": "",
+    "ddd": "21",
+    "siafi": "6001"
+   }
